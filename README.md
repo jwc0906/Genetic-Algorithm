e4ff045d5a136b47a6e966e23920590cd9afc9d3
jwc0906 <yhjgoldhair@naver.com>
2019-10-23 00:23:50 +0900

    genetic algorithm v0

---
+++ README.md
@@ -0,0 +1,4 @@
+# Genetic Algorithm
+
+### example code
+<pre><code>mse_loss, x= gen_algorithm(model=model_example, y_hat=y_hat, x_len=5, epoch=100)</code></pre>
---
+++ genetic_algorithm.py
@@ -0,0 +1,85 @@
+# 최적의 sequence
+import numpy as np
+#np.array([])
+
+def sampling_tf(true_p, num):
+    f_t=np.array([False, True])
+    idx= np.random.choice(2, num, p= [1-true_p, true_p])
+    idx_ft=f_t[idx]
+
+    return idx_ft
+
+# y_hat, input of model, output of model 의 데이터타입은 모두  numpy array (float)
+def gen_algorithm(model, y_hat, x_len=5, epoch=100, p_crossover= 0.2, p_mutation= 0.2, descendants=500, max_p_clip=0.5, visible= True):
+    #init
+    gen= np.random.uniform(0, 1, [descendants,x_len])
+    for ep in range(epoch):
+        # loss 작을수록 sampling 확률 up
+        y_pred= model(gen)
+
+        loss= (y_pred - y_hat[0])**2
+        sample_p= 1/ loss.reshape(loss.shape[0], 1)
+        sample_p= sample_p/sample_p.sum()
+        sample_p= np.clip(sample_p, 0, max_p_clip)
+        sample_p= sample_p/sample_p.sum()
+
+        idx= loss.argsort(axis=0)
+        sample_p= sample_p[idx].reshape(-1)
+        #print(sample_p)
+        #print("")
+        gen_sorted= gen[idx]
+        loss_sorted= loss[idx]
+
+
+        next_gen=None
+        for i in range(gen_sorted.shape[0]//2):
+            idx= np.random.choice(gen_sorted.shape[0], 2, p=sample_p, replace=False) # loss의 역수의 확률로 두개의 부모 뽑기
+            gen_tmp= gen_sorted[idx]
+
+            idx_ft= sampling_tf(p_crossover, x_len)
+
+            #교차 실행
+            for j in range(x_len):
+                if idx_ft[j]==True:
+                    tmp= gen_tmp[0, j]
+                    gen_tmp[0, j]= gen_tmp[1, j]
+                    gen_tmp[1, j]= tmp
+
+            #두 자식을 next_gen 으로 전달
+            if i==0:
+                next_gen= gen_tmp
+                #print("aa", next_gen.shape)
+            else:
+                next_gen= np.vstack([next_gen, gen_tmp])
+
+
+        #돌연변이
+        idx_ft= sampling_tf(p_crossover, [next_gen.shape[0],next_gen.shape[1]])
+        random_gen= np.random.uniform(0, 1, [next_gen.shape[0],next_gen.shape[1]])
+
+        next_gen= np.logical_not(idx_ft) * next_gen + idx_ft * random_gen
+
+
+        # next_gen 이 현재의 gen이 됨
+        gen= next_gen
+        if (ep%(epoch//10)==0 or ep==epoch-1 ) and visible==True:
+            #print("[", ep, "]", "loss:", loss_sorted[0], "gen", gen_sorted[0])
+            print("[{0:5d}] loss:{1:15.20f} gen:".format(ep, loss_sorted[0]), gen_sorted[0])
+
+    return loss_sorted[0], gen_sorted[0]
+
+
+# 사용 예
+if __name__=="__main__":
+
+    def model_example(x):
+        w_= np.array([[1, 5, 3, -1, -3]])
+        result= np.sum(x * w_, axis=1)
+        return result
+
+    y_hat= np.array([3])
+
+    mse_loss, x= gen_algorithm(model=model_example, y_hat=y_hat, x_len=5, epoch=100)
+
+    print(mse_loss)
+    print(x)
